61b15936c0b19648b314081ad4332c5d96c11a90
dimcoord <dimastio20062@gmail.com>
2024-12-26 16:49:23 +0700

    feat: added web UI

--- main.py
+++ main.py
@@ -1,82 +1,81 @@
+from flask import Flask, render_template, request, redirect
 from PIL import Image, ImageDraw
 from collections import Counter
-from io import BytesIO
 from bs4 import BeautifulSoup
+from io import BytesIO
 import requests
-import json
+import base64
+import io
 
-exclude_dirs = "assets, public "
-exclude_files = " .json, .xml, .yaml, .yml, .ini, .config, .md, .txt, .rst, .png, .jpg, .jpeg, .gif, .bmp, .svg, .ico, .zip, .tar, .gz, .rar, .7z, .log, .pdf, .doc, .docx, .xls, .xlsx, .gitignore, LICENSE"
-exclude = exclude_files.replace(" ", "")
-print(exclude)
+app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/")
 
-# Define the URL you want to send the GET request to
-username = input("Input username Github: ") or " "
+def fetchProfileInfo(username):
+    exclude_dirs = "assets, public "
+    exclude_files = " .json, .xml, .yaml, .yml, .ini, .config, .md, .txt, .rst, .png, .jpg, .jpeg, .gif, .bmp, .svg, .ico, .zip, .tar, .gz, .rar, .7z, .log, .pdf, .doc, .docx, .xls, .xlsx, .gitignore, LICENSE"
+    exclude = exclude_files.replace(" ", "")
 
-# Send the GET request
-html_source = requests.get(f"https://github.com/{username}?tab=repositories").text
-soup = BeautifulSoup(html_source, "lxml")
-profile = soup.find_all("li", class_="col-12 d-flex flex-justify-between width-full py-4 border-bottom color-border-muted public source")
+    # Send the GET request
+    html_source = requests.get(f"https://github.com/{username}?tab=repositories").text
+    soup = BeautifulSoup(html_source, "lxml")
+    profile = soup.find_all("li", class_="col-12 d-flex flex-justify-between width-full py-4 border-bottom color-border-muted public source")
 
-# Check the status code of the response
-if html_source:
-    repo_list = []
-    new_repo_list = []
-    timestamps = []
-    languages = []
+    # Check the status code of the response
+    if html_source:
+        repo_list = []
+        new_repo_list = []
+        timestamps = []
+        languages = []
 
-    for i in profile:
-        timestamps.append(i.find("relative-time").text)
-        last_updated = i.find("relative-time").text
-        if "4" == last_updated[-1]:
-            new_repo_list.append(i.find("a"))
-        elif "3" == last_updated[-1]:
-            repo_list.append(i.find("a"))
+        for i in profile:
+            timestamps.append(i.find("relative-time").text)
+            last_updated = i.find("relative-time").text
+            if "4" == last_updated[-1]:
+                new_repo_list.append(i.find("a"))
+            elif "3" == last_updated[-1]:
+                repo_list.append(i.find("a"))
 
-    total = 0
-    new_total = 0
+        total = 0
+        new_total = 0
 
-    print("This year:")
-    for html_tag in new_repo_list:
-        repo_name = html_tag.get("href")
-        response = requests.get(f"https://api.codetabs.com/v1/loc?github={repo_name[1:]}&ignored={exclude}")
-        data = response.json()
+        print("This year:")
+        for html_tag in new_repo_list:
+            repo_name = html_tag.get("href")
+            response = requests.get(f"https://api.codetabs.com/v1/loc?github={repo_name[1:]}&ignored={exclude}")
+            data = response.json()
 
-        # Check if the response is a list
-        if isinstance(data, list):
-            languages.extend([item['language'] for item in data[:-1]])
-            lines_of_code = data[-1]['linesOfCode']
-            new_total += lines_of_code
-            print(f"LOC of {repo_name}: {lines_of_code}")
-        else:
-            print(f"Unexpected response format for repository {repo_name}: {data}")
+            # Check if the response is a list
+            if isinstance(data, list):
+                languages.extend([item['language'] for item in data[:-1]])
+                lines_of_code = data[-1]['linesOfCode']
+                new_total += lines_of_code
+                print(f"LOC of {repo_name}: {lines_of_code}")
+            else:
+                print(f"Unexpected response format for repository {repo_name}: {data}")
 
-    print("Last year:")
-    for html_tag in repo_list:
-        repo_name = html_tag.get("href")
-        response = requests.get(f"https://api.codetabs.com/v1/loc?github={repo_name[1:]}&ignored={exclude}")
-        data = response.json()
+        print("Last year:")
+        for html_tag in repo_list:
+            repo_name = html_tag.get("href")
+            response = requests.get(f"https://api.codetabs.com/v1/loc?github={repo_name[1:]}&ignored={exclude}")
+            data = response.json()
 
-        # Check if the response is a list
-        if isinstance(data, list):
-            languages.extend([item['language'] for item in data[:-1]])
-            lines_of_code = data[-1]['linesOfCode']
-            total += lines_of_code
-            print(f"LOC of {repo_name}: {lines_of_code}")
-        else:
-            print(f"Unexpected response format for repository {repo_name}: {data}")
+            # Check if the response is a list
+            if isinstance(data, list):
+                languages.extend([item['language'] for item in data[:-1]])
+                lines_of_code = data[-1]['linesOfCode']
+                total += lines_of_code
+                print(f"LOC of {repo_name}: {lines_of_code}")
+            else:
+                print(f"Unexpected response format for repository {repo_name}: {data}")
 
-    print('You wrote ', new_total, ' lines of code this year!')
-    print('And also ', total, ' lines of code last year')
-else:
-    # If the request was not successful, print the status code
-    print('Failed to retrieve data')
-    print('Status code:', response.status_code)
+        print('You wrote ', new_total, ' lines of code this year!')
+        print('And also ', total, ' lines of code last year')
+    else:
+        # If the request was not successful, print the status code
+        print('Failed to retrieve data')
+        print('Status code:', response.status_code)
 
-counter = Counter(languages)
-fav_lang = counter.most_common()[:5]
-
-def create_image_with_text(output_filename="output.png"):
+    counter = Counter(languages)
+    fav_lang = counter.most_common()[:5]
 
     image_url = "https://drive.usercontent.google.com/u/1/uc?id=1dS9vOVdmAe6_EVZa1foetLGcfQ2BTIYY&export=download"
     response = requests.get(image_url)
@@ -95,8 +94,28 @@ def create_image_with_text(output_filename="output.png"):
         d.text((100, 400+(50*mult)), f"{i[0]}", fill="white", font_size=30)
         mult += 1
 
-    # Save the image
-    display(img)
+    # Save the image to a BytesIO object
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
 
-# Example usage
-create_image_with_text("hello_world.png")
+    # Return the image
+    img_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
+    return img_base64
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    if request.method == "POST":
+        username = request.form["username"]
+        if username:
+            output = fetchProfileInfo(username)
+            return render_template('result.html', output=output)
+    else:
+        return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
